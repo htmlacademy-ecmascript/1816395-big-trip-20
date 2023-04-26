f2ac9d2ff93fcb0e92ac7c3a8159feb32fc4e8be
evilJO <eviljosephe@gmail.com>
2023-04-27 00:07:31 +0300

    [feature] add tripEventItem

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import { TripFiltersView } from './view/trip-filters-trip-view.js';
 import { SortsEventsTripView } from './view/sort-events-trip-view.js';
 import { AddNewPointView } from './view/add-new-point-view.js';
 import { EditPointView } from './view/edit-point-view.js';
+import { TripEventsItemView } from './view/trip-events-item-view.js';
 import { render } from './render.js';
 
 const siteBodyElement = document.querySelector('body');
 const siteHeaderElement = siteBodyElement.querySelector('.page-header');
 const filterContainerElement = siteHeaderElement.querySelector('.trip-controls__filters');
 const tripEventsElement = siteBodyElement.querySelector('.trip-events');
-// const tripEventContainer = siteBodyElement.querySelector('.trip-events__list');
+const tripEventContainerElement = siteBodyElement.querySelector('.trip-events__list');
 
 render(new TripFiltersView(), filterContainerElement);
-render(new SortsEventsTripView(), tripEventsElement);
-render(new AddNewPointView(), tripEventsElement);
-render(new EditPointView(), tripEventsElement);
+render(new SortsEventsTripView(), tripEventsElement, 'afterbegin');
+render(new AddNewPointView(), tripEventContainerElement);
+render(new EditPointView(), tripEventContainerElement);
+render(new TripEventsItemView(),tripEventContainerElement);
+render(new TripEventsItemView(),tripEventContainerElement);
+render(new TripEventsItemView(),tripEventContainerElement);
 
