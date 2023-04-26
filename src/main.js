b0957fdabef31bdf7cd9cb23d0e272b92943d21b
evilJO <eviljosephe@gmail.com>
2023-04-26 23:18:12 +0300

    [feature] add sort trip event

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { TripFiltersView } from './view/trip-filters-view.js';
+import { TripFiltersView } from './view/trip-filters-trip-view.js';
+import { SortsEventsTripView } from './view/sort-events-trip-view.js';
 import { render } from './render.js';
 
-const siteMainElement = document.querySelector('body');
-const siteHeaderElement = siteMainElement.querySelector('.page-header');
+const siteBodyElement = document.querySelector('body');
+const siteHeaderElement = siteBodyElement.querySelector('.page-header');
 const filterContainerElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const tripEventsElement = siteBodyElement.querySelector('.trip-events');
 
 render(new TripFiltersView(), filterContainerElement);
+render(new SortsEventsTripView(),tripEventsElement);
 
