157abdfe58d79ee27055de5d5257075e6c530cc7
evilJO <eviljosephe@gmail.com>
2023-05-02 00:27:00 +0300

    [update] change model to mock array

--- src/model/trip-points-model.js
+++ src/model/trip-points-model.js
@@ -1,4 +1,5 @@
 import { CONST_DATA } from '../mock/const-data.js';
+import { getTripPoints } from '../mock/mock-data.js';
 import { getRandomTripPoint } from '../mock/mock-data.js';
 
 import { util } from '../util.js';
@@ -6,7 +7,8 @@ import { util } from '../util.js';
 const POINTS_COUNT = util.getRandomCount(CONST_DATA.countLimit);
 
 export default class TripPointsModel {
-  tripPoints = Array.from({ length: POINTS_COUNT }, getRandomTripPoint);
+  // tripPoints = Array.from({ length: POINTS_COUNT }, getRandomTripPoint);
+  tripPoints = getTripPoints();
 
   getPoints() {
     return this.tripPoints;
