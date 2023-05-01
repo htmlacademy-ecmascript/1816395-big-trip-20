389db83893a5fa577bfa2f13693c0b3e330a7de3
evilJO <eviljosephe@gmail.com>
2023-05-02 00:19:02 +0300

    [feature] model connected in pointPresenter

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -5,8 +5,9 @@ import { render } from '../render.js';
 export default class PointPresenter {
   pointComponent = new TripEventsItemView();
 
-  constructor({pointContainer}) {
+  constructor({ pointContainer, tripPointsModel }) {
     this.pointContainer = pointContainer;
+    this.tripPointsModel = tripPointsModel;
   }
 
   init() {
