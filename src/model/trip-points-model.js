562af3412a7f7da1a41264c26d27a29832608210
evilJO <eviljosephe@gmail.com>
2023-05-02 00:02:14 +0300

    [fix] renamed mock data function

--- src/model/trip-points-model.js
+++ src/model/trip-points-model.js
@@ -1,11 +1,12 @@
 import { CONST_DATA } from '../mock/const-data.js';
-import { getRandomTask } from '../mock/task.js';
+import { getNewTripPoint } from '../mock/mock-data.js';
+
 import { util } from '../util.js';
 
 const POINTS_COUNT = util.getRandomCount(CONST_DATA.countLimit);
 
 export default class TripPointsModel {
-  tripPoints = Array.from({ length: POINTS_COUNT }, getRandomTask);
+  tripPoints = Array.from({ length: POINTS_COUNT }, getNewTripPoint);
 
   getPoints() {
     return this.tripPoints;
