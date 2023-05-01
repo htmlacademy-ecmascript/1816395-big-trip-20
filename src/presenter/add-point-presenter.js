a63dca41c7379e238a30a3aa824b6b3c264359bb
evilJO <eviljosephe@gmail.com>
2023-05-02 00:36:22 +0300

    [feature] sent data to component

--- src/presenter/add-point-presenter.js
+++ src/presenter/add-point-presenter.js
@@ -3,15 +3,14 @@ import AddNewPointView from '../view/add-new-point-view.js';
 import { render } from '../render.js';
 
 export default class AddPointPresenter {
-  addPointComponent = new AddNewPointView();
 
-  constructor({pointContainer,tripPoints}) {
+  constructor({ pointContainer, tripPoints }) {
     this.pointContainer = pointContainer;
     this.tripPoints = tripPoints;
   }
 
   init() {
-    render(this.addPointComponent, this.pointContainer);
+    render(new AddNewPointView({ point: this.tripPoints[0] }), this.pointContainer);
   }
 
 }
