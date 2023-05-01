151e0be14a04498e28ef270f2fd29b14be304fa5
evilJO <eviljosephe@gmail.com>
2023-05-02 00:10:51 +0300

    [fix] fixed recursion

--- src/model/trip-points-model.js
+++ src/model/trip-points-model.js
@@ -1,12 +1,12 @@
 import { CONST_DATA } from '../mock/const-data.js';
-import { getNewTripPoint } from '../mock/mock-data.js';
+import { getRandomTripPoint } from '../mock/mock-data.js';
 
 import { util } from '../util.js';
 
 const POINTS_COUNT = util.getRandomCount(CONST_DATA.countLimit);
 
 export default class TripPointsModel {
-  tripPoints = Array.from({ length: POINTS_COUNT }, getNewTripPoint);
+  tripPoints = Array.from({ length: POINTS_COUNT }, getRandomTripPoint);
 
   getPoints() {
     return this.tripPoints;
