83469664762a3b2073daefc91797984156738234
evilJO <eviljosephe@gmail.com>
2023-05-01 23:44:55 +0300

    [update] renamed class of model

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -2,12 +2,12 @@ import { CONST_DATA } from '../mock/const-data.js';
 import { getRandomTask } from '../mock/task.js';
 import { util } from '../util.js';
 
-const TASK_COUNT = util.getRandomCount(CONST_DATA.countLimit);
+const POINTS_COUNT = util.getRandomCount(CONST_DATA.countLimit);
 
-export default class TasksModel {
-  tasks = Array.from({ length: TASK_COUNT }, getRandomTask);
+export default class TripPoints {
+  tripPoints = Array.from({ length: POINTS_COUNT }, getRandomTask);
 
-  getTasks() {
-    return this.tasks;
+  getPoints() {
+    return this.tripPoints;
   }
 }
