7739fb83a5ae7348ed056ce8b3ba04b7fcf480f5
evilJO <eviljosephe@gmail.com>
2023-05-05 15:52:10 +0300

    [feature] add generateEventTypeBtn()

--- src/view/add-new-point-view.js
+++ src/view/add-new-point-view.js
@@ -1,18 +1,25 @@
 import { createElement } from '../render.js';
 
 function createAddNewPointTemplate() {
+
+  const generateEventTypeBtn = () => (/*html*/
+    `
+      <label class="event__type  event__type-btn" for="event-type-toggle-1">
+        <span class="visually-hidden">Choose event type</span>
+        <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+      </label>
+    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+    `);
+
+
+
   return (/*html*/
     `
     <li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
                   <div class="event__type-wrapper">
-                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
-                      <span class="visually-hidden">Choose event type</span>
-                      <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
-                    </label>
-                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
+                    ${generateEventTypeBtn()}
                     <div class="event__type-list">
                       <fieldset class="event__type-group">
                         <legend class="visually-hidden">Event type</legend>
