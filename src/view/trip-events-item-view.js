8f4a1047eea42c046ed5c9445a2ee7fbc6df1148
evilJO <eviljosephe@gmail.com>
2023-05-12 14:18:35 +0300

    [fix] regulated TELview

--- src/view/trip-events-item-view.js
+++ src/view/trip-events-item-view.js
@@ -1,70 +1,77 @@
 import { createElement } from '../render.js';
 import { util } from '../util.js';
 
+function createOfferHTML(offer) {
+  return (/*html*/
+    `
+  <li class="event__offer">
+    <span class="event__offer-title">${offer.title}</span>
+    &plus;&euro;&nbsp;
+    <span class="event__offer-price">${offer.price}</span>
+  </li>
+`
+  );
+}
+
+function createOffersHtml(offers) {
+  return offers
+    .map((offer) => createOfferHTML(offer))
+    .join('');
+}
+
+
 function createTripEventItemTemplate(tripPoint, destinationList, offersList) {
 
-  const generateFavorite = (isFavorite) => {
-    if (isFavorite) {
-      return 'event__favorite-btn--active';
-    } else {
-      return '';
-    }
+  const
+    dateStartTrip = tripPoint.dateFrom,
+    dateEndTrip = tripPoint.dateTo;
 
-  };
+  const
+    dateInfoStartTrip = util.humanizeDateInfo(dateStartTrip),
+    datePointStartTrip = util.humanizeDatePoint(dateStartTrip),
+    datePointEndTrip = util.humanizeDatePoint(dateEndTrip),
+    tripExtension = util.getPeriodExtension(tripPoint);
 
 
-  const generateOfferTag = (offer) => {
+  const
+    favoriteClassName = tripPoint.isFavorite
+      ? 'event__favorite-btn--active'
+      : [];
 
-    const tagOffer = (/*html*/
-      `
-      <li class="event__offer">
-        <span class="event__offer-title">${offer.title}</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">${offer.price}</span>
-      </li>
-    `
-    );
+  const
+    type = tripPoint.type,
+    tripPrice = tripPoint.basePrice;
 
-    return tagOffer;
-  };
-
-  const generateOffersTag = () => {
-    let offersTag = '';
-    const offersById = util.getOffersById(tripPoint.type, tripPoint.offers, offersList);
-    offersById.forEach((offer) => {
-      const offerTag = generateOfferTag(offer);
-      offersTag = offersTag + offerTag;
-    });
-
-    return offersTag;
-  };
-
+  const
+    destinationName = util.getDestinationById(tripPoint.destination, destinationList).name,
+    offersById = util.getOffersById(tripPoint.type, tripPoint.offers, offersList),
+    offersHtml = createOffersHtml(offersById);
 
   return (/*html*/
     `
     <li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">${util.humanizeDateInfo(tripPoint.dateFrom)}</time>
+      <time class="event__date" datetime="${dateStartTrip}">${dateInfoStartTrip}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${tripPoint.type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">${tripPoint.type} ${util.getDestinationById(tripPoint.destination, destinationList).name}</h3>
+      <h3 class="event__title">${type} ${destinationName}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="${tripPoint.dateFrom}">${util.humanizeDatePoint(tripPoint.dateFrom)}</time>
+          <time class="event__start-time" datetime="${dateStartTrip}">${datePointStartTrip}</time>
           &mdash;
-          <time class="event__end-time" datetime="${tripPoint.dateTo}">${util.humanizeDatePoint(tripPoint.dateTo)}</time>
+          <time class="event__end-time" datetime="${dateEndTrip}">${datePointEndTrip}</time>
         </p>
-        <p class="event__duration">${util.getPeriodExtension(tripPoint)}</p>
+        <p class="event__duration">${tripExtension}</p>
       </div>
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">${tripPoint.basePrice}</span>
+        &euro;&nbsp;<span class="event__price-value">${tripPrice}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        ${generateOffersTag()}
+        ${offersHtml}
       </ul>
-      <button class="event__favorite-btn ${generateFavorite(tripPoint.isFavorite)}" type="button">
+      <button class="event__favorite-btn ${favoriteClassName}" type="button">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
           <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
